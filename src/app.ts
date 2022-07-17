311e26663b56ad2dccba68937e6dc27fdb9f39cb
mecamon <arq.camejia@gmail.com>
2022-07-17 17:22:48 -0400

    ADDED DOCKER TO DEV PROCESS

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send({ message: "Allo! Catch-all route." });
+  res.send({ message: "Allo! Catch-all routes." });
 });
 
 export default app
